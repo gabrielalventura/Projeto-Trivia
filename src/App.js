efef94b3fc6091d3c2f22f26a5a5258c52a43666
EliabeRP <eliabe.rodrgu3s@gmail.com>
2022-11-09 17:16:42 -0300

    Req 1 - Inicio pagina login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import Login from './pages/Login';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>SUA VEZ</p>
       </header>
+
+      <Switch>
+        <Route path="/" component={ Login } />
+      </Switch>
     </div>
   );
 }
+
+export default App;
