ba3ca1bb7f3b9e814fd22583eabe980da7b24271
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2022-11-11 22:35:51 -0300

    11 pronta

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Login from './pages/Login';
 import ScreenGame from './pages/ScreenGame';
 import SettingsPage from './pages/SettingsPage';
+import Feedback from './pages/Feedback';
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ ScreenGame } />
         <Route path="/settings" component={ SettingsPage } />
-
+        <Route path="/feedback" component={ Feedback } />
       </Switch>
     </div>
   );
