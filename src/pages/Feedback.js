cd9a63d189a35df2e5bf3eb1d806882ac7a03716
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-11-11 23:22:29 -0300

    Finalização do requisito 12, inserindo o header na página de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import HeaderGame from '../components/HeaderGame';
 
 class Feedback extends React.Component {
   render() {
     return (
       <div>
+        <header>
+          <HeaderGame />
+        </header>
         <h1 data-testid="feedback-text">
           FEEDBACK
-
         </h1>
       </div>
     );
