ee9403698e3c4b4ea87d8fdae3689393fe47f8c8
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2022-11-12 22:44:19 -0300

    Quando Carla fizer 16 a 17 ta pronta

--- src/tests/Feedback.test.js
+++ src/tests/Feedback.test.js
@@ -1,11 +1,27 @@
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
-import Feedback from '../pages/Feedback';
+import App from "../App";
 
 describe('Testa pagina feedback', () => {
+    const playerA = {
+        player: {
+            name: 'João',
+            assertions: 3,
+            score: 4,
+            gravatarEmail: 'j@hot.com'
+        }
+    }
+    const playerB = {
+        player: {
+            name: 'Eliabe',
+            assertions: 2,
+            score: 4,
+            gravatarEmail: 'Eliabe@hot.com'
+        }
+    }
   it('Test se os datatestid estão no document', () => {
-   renderWithRouterAndRedux(<Feedback />);
+   renderWithRouterAndRedux(<App />, playerA, '/feedback');
 
     const score = screen.getByTestId('feedback-total-score')
     expect(score).toBeInTheDocument();
@@ -20,21 +36,35 @@ describe('Testa pagina feedback', () => {
     
 
     });
-  it('teste botões', () => {
-    const { history } = renderWithRouterAndRedux(<Feedback />);
+  it('teste botao Play Again', () => {
+    const { history } = renderWithRouterAndRedux(<App />, playerA, '/feedback');
 
-    const play = screen.getByRole('button', { name: /Play/i });
+    const play = screen.getByRole('button', { name: /Play Again/i });
         
     expect(play).toBeInTheDocument();
     expect(play).toBeEnabled();
     userEvent.click(play);
 
     expect(history.location.pathname).toBe('/')
-    // const btn = screen.getByTestId('btn-ranking')
-    // expect(btn).toBeInTheDocument();
-    // userEvent.click(btn);
-    // expect(history.location.pathname).toBe('/ranking')
-    // const ranking = screen.getByTestId('ranking-title')
+   
   });
+//   test('teste botao ranking', () => {
+//     const { history } = renderWithRouterAndRedux(<App />, playerA, '/feedback');
+//    const btn = screen.getByTestId('btn-ranking')
+//     expect(btn).toBeInTheDocument();
+//     userEvent.click(btn);
+//     expect(history.location.pathname).toBe('/ranking')
+//     const ranking = screen.getByTestId('ranking-title')
+//   });
+  test('testa mensagem boa', () => {
+    const { getByText } = renderWithRouterAndRedux(<App />, playerA, '/feedback')
+    const wellDone = getByText(/well done!/i)
+    expect(wellDone).toBeInTheDocument()
+  });
+  test('testa mensagem ruim', () => {
+    const { getByText } = renderWithRouterAndRedux(<App />, playerB, '/feedback')
+    const wellDone = getByText(/Could be better.../i)
+    expect(wellDone).toBeInTheDocument()
+  })
     
 });
